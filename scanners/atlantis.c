a1be8e76c745c313bb06fcc0453b54bde8d4f1bd
Geert-Jan77 <95014585+Geert-Jan77@users.noreply.github.com>
2024-11-05 03:35:17 +0100

    Add files via upload

--- scanners/atlantis.c
+++ scanners/atlantis.c
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------
-  aces.c (Ace of Aces)
-
+  atlantis.c (Atlantis Loader)
+  
   Part of project "Final TAP". 
   
   A Commodore 64 tape remastering and data extraction utility.
@@ -27,101 +27,94 @@
 #include "../mydefs.h"
 #include "../main.h"
 
-#define HDSZ 20
+#define HDSZ 6
 
 /*---------------------------------------------------------------------------*/
-void aces_search(void)
+void atlantis_search(void)
 {
-   int i,j,sof,sod,eod,eof;
-   int z,x,hd[HDSZ];
+   int i,sof,sod,eod,eof;
+   int z,h,hd[HDSZ];
+   unsigned int s,e,x;
 
    if(!quiet)
-      msgout("  Ace of Aces tape");
-   
-   for(i=20; i<tap.len; i++)
+      msgout("  Atlantis Tape");
+
+   for(i=20; i<tap.len-8; i++)
    {
-      if((z=find_pilot(i,ACES))>0)
+      if((z=find_pilot(i,ATLAN))>0)
       {
-         sof=i;
-         i=z;
-         if(readttbyte(i, ft[ACES].lp, ft[ACES].sp, ft[ACES].tp, ft[ACES].en)==ft[ACES].sv)
+         sof= i;
+         i= z;
+         if(readttbyte(i, ft[ATLAN].lp, ft[ATLAN].sp, ft[ATLAN].tp, ft[ATLAN].en)==ft[ATLAN].sv)
          {
-            sod= i+8;
+            sod= i+8 +8;  /* extra +8 skips sync2 ($42)  */
+            /* decode the header so we can validate the addresses...  */
+            for(h=0; h<HDSZ; h++)
+               hd[h] = readttbyte(sod+(h*8), ft[ATLAN].lp, ft[ATLAN].sp, ft[ATLAN].tp, ft[ATLAN].en);
 
-            /* decode header to get the file size */
-            for(j=0; j<HDSZ; j++)
-               hd[j]= readttbyte(sod+(j*8), ft[ACES].lp, ft[ACES].sp, ft[ACES].tp, ft[ACES].en);
-            x= (hd[2]+(hd[3]<<8)) - (hd[0]+(hd[1]<<8));
-
-            if(x>0)
+            s= hd[5]+(hd[4]<<8);   /* get start address */
+            e= hd[3]+(hd[2]<<8);   /* get end address   */
+            if(e>s)
             {
-               eod= sod+(x*8)+(HDSZ*8);  
+               x= e-s;
+               eod= sod+((x+HDSZ)*8) +8;    /* note +8 points eod at trailing zero. */
                eof= eod+7;
-               addblockdef(ACES, sof,sod,eod,eof, 0);
-               i= eof;  /* optimize search   */
+               addblockdef(ATLAN, sof,sod,eod,eof, 0);
+               i= eof;  /* optimize search */
             }
          }
       }
       else
       {
-         if(z<0)    /* find_pilot failed (too few/many), set i to failure point.   */
+         if(z<0)    /* find_pilot() failed (too few/many), set i to failure point. */
             i=(-z);
       }
    }
 }
 /*---------------------------------------------------------------------------*/
-int aces_describe(int row)
+int atlantis_describe(int row)
 {
-   int i,s;
-   int hd[HDSZ],b,rd_err,cb;
-   char fn[256],str[2000]="";
+   int i,s,b,hd[HDSZ],rd_err,cb;
 
-   /* decode header...   */
+   /* decode header... */
    s= blk[row]->p2;
    for(i=0; i<HDSZ; i++)
-      hd[i]= readttbyte(s+(i*8), ft[ACES].lp, ft[ACES].sp, ft[ACES].tp, ft[ACES].en);
+      hd[i]= readttbyte(s+(i*8), ft[ATLAN].lp, ft[ATLAN].sp, ft[ATLAN].tp, ft[ATLAN].en);
    
-   blk[row]->cs= hd[0]+ (hd[1]<<8);                  /* record start address */
-   blk[row]->ce= hd[2]+ (hd[3]<<8) -1;               /* record end address   */
-   blk[row]->cx= (blk[row]->ce +1) - blk[row]->cs;   /* record length        */
+   blk[row]->cs= hd[5]+(hd[4]<<8);
+   blk[row]->ce= hd[3]+(hd[2]<<8)-1;
+   blk[row]->cx= (blk[row]->ce - blk[row]->cs)+1;  
 
-   /* extract file name... */
-   for(i=0; i<16; i++)
-      fn[i]= hd[4+i];
-   fn[i]=0;
-   trimstring(fn);
-   pet2text(str,fn);
-   if(blk[row]->fn!=NULL)
-      free(blk[row]->fn);
-   blk[row]->fn = (char*)malloc(strlen(str)+1);
-   strcpy(blk[row]->fn, str);
-
-   /* get pilot & trailer lengths   */
+   /* get pilot trailer lengths   */
    blk[row]->pilot_len= (blk[row]->p2 - blk[row]->p1 -8)>>3;
    blk[row]->trail_len= (blk[row]->p4 - blk[row]->p3 -7)>>3;
 
-   /* extract data and test checksum...  */
+   /* extract data and test checksum... */
    rd_err=0;
    cb=0;
-   s= (blk[row]->p2)+(HDSZ*8);
+   s= blk[row]->p2+(HDSZ*8);
 
    if(blk[row]->dd!=NULL)
       free(blk[row]->dd);
-   blk[row]->dd= (unsigned char*)malloc(blk[row]->cx);
+   blk[row]->dd = (unsigned char*)malloc(blk[row]->cx);
 
    for(i=0; i<blk[row]->cx; i++)
    {
-      b= readttbyte(s+(i*8), ft[ACES].lp, ft[ACES].sp, ft[ACES].tp, ft[ACES].en);
+      b= readttbyte(s+(i*8), ft[ATLAN].lp, ft[ATLAN].sp, ft[ATLAN].tp, ft[ATLAN].en);
       cb^=b;
       if(b==-1)
          rd_err++;
       blk[row]->dd[i]=b;
    }
-   b= readttbyte(s+(i*8), ft[ACES].lp, ft[ACES].sp, ft[ACES].tp, ft[ACES].en);
+   b= readttbyte(s+(i*8), ft[ATLAN].lp, ft[ATLAN].sp, ft[ATLAN].tp, ft[ATLAN].en); /* read actual checkbyte. */
 
    blk[row]->cs_exp= cb &0xFF;
    blk[row]->cs_act= b;
    blk[row]->rd_err= rd_err;
+
    return 0;
 }
 
+
+
+ 
